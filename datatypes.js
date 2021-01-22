e238064c3ba88d93fe38193c82d4c2dc0c866de9
yyk <yyk@mail.ru>
2021-01-22 06:34:03 -0400

    data types view enhanced

--- datatypes.js
+++ datatypes.js
@@ -1,6 +1,6 @@
 /*
 
-//data DayDescription = {date:Date,
+data DayDescription = {date:Date,
                          day_of_month::[1-31],
                          day_of_week:Int,
                          weekday_of_month::[0-5],
@@ -18,24 +18,25 @@ data GeneratorSlot = {slot_id: Id,
                       rounds_left:Int,
                       repeat: always|finite}
 
-//data Trigger = {type::TriggerType, start_date::Iso8601d}
-//type GroupId = Int
-//data TriggerType = A|B|C|...|Z
-//data GroupedTriggerSlots = {<x::TriggerSlotType>:[Trigger]}
-//data Repetition = Times Number | Always
-//data TriggerSlot = {slot_id::Int, trigger_group_id::Int, start_date::Date, interval::Int, brief::String, description::String, parent_code::Int,
-//lifetime::Int, active::Bool, repeat::Repeatition, timestamp::UnixMs}
-//data UnixMs = Int //unix time in milliseconds
-//data Iso8601d = YYYY-MM-DD
-//data JsInterval = {from:Date, to: Date}
-//date Period = [Iso8601d, Iso8601d]
-//data TimeStructure = {ms:Int, seconds:Int, minutes:Int, hours:Int, days:Int, weeks:Int}
-//data NicePeriod = String
-//data FormatedTime = hh:mm
-//data ExtraShort = D-MMM //3-May, 30-Jun
-//data Short = D<suffix>-MMM //3-rd May, 30-th Jun
-//data AdwordsReportDate = YYYYMMDD //20170503, 20170630
-//data Timezone = Timezone
-//data Seconds = Int
-//data YearMonth = YYYY-MM
+data UnixMs = Int //unix time in milliseconds
+data Iso8601d = YYYY-MM-DD
+data TriggerType = A|B|C|...|Z
+data Trigger = {type::TriggerType, start_date::Iso8601d}
+type GroupId = Int
+
+data Repetition = Times Number | Always
+//data TriggerSlot = {slot_id::Int,
+                      trigger_group_id::Int,
+                      start_date::Date,
+                      interval::Int,
+                      brief::String,
+                      description::String,
+                      parent_code::Int,
+                      lifetime::Int,
+                      active::Bool,
+                      repeat::Repeatition,
+                      timestamp::UnixMs}
+
+data GroupedTriggerSlots = {<x::TriggerSlotType>:[Trigger]}
+
 */
